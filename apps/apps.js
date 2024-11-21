991430b1d26663dcf666b26efb4500a8ccf0cfbd
noahmercier123 <noah.mercier.541@k12.friscoisd.org>
2024-11-20 21:06:32 -0600

    Add files via upload

---
+++ apps/apps.js
@@ -0,0 +1,14 @@
+// Check if user is authenticated
+if (!sessionStorage.getItem('authenticated')) {
+    window.location.href = '../password.html'; // Redirect to password page if not authenticated
+}
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 1000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
